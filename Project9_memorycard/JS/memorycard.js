d25d6a7a3b99af83726af8bd3886d43e91a415f0
congtrinh0209 <congtrinh0209@gmail.com>
2017-04-06 17:27:52 +0700

    up

--- Project9_memorycard/JS/memorycard.js
+++ Project9_memorycard/JS/memorycard.js
@@ -1,13 +1,6 @@
 var card = ["1", "2", "3", "4", "5", "6", "11", "12", "9"];
 var time = 60;
-
-/*function remaining_time(){
-	setInterval(function(){
-	time--;
-	document.getElementById('').value = time;
-	},1000)
-}*/
-
+/********     Đảo mảng     **********/
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
   while (0 !== currentIndex) {
@@ -19,6 +12,40 @@ function shuffle(array) {
   }
   return array;
 }
+/********    Gán dữ liệu    **********/
+$(function(){
+	card = card.concat(card);
+	card = shuffle(card);
+	var display = "";
+	for (var i = 0; i < card.length; i++) {
+		display += '<div class="content">' + '<div class="card" data-name="'+card[i]+'" onclick = "flip(this)">'+'<div class="back-card"><img src="../IMG/'+card[i]+'.jpg"></div>'+'<div class="front-card"><img src="../IMG/card-back2.jpg"></div>'+'</div></div>'};			
+	$("#wrapper").html(display);
+	$('.card').css('pointer-events', 'none');
+});
+/********   Click Start-bắt đầu chạy thời gian    **********/
+var run_time;
+$('.time_number').children('h3').html(time);
+$('.start').click(function(){
+	document.getElementById('open-music').pause();
+	document.getElementById('start-music').play();
+	document.getElementById('ost').play();
+	$('.time').css('display','block');
+	$('.start').css('display','none');
+	$('.card').css({'pointer-events':'auto','opacity':'1'});
+	run_time = setInterval(function(){	/*********LOSE********/
+		time--;
+		$('.time_number').children('h3').html(time);    /* màu thời gian */
+		if(time<30){$('.time_number').children('h3').css('color','yellow')};
+		if(time<10){$('.time_number').children('h3').css('color','red');};
+		if(time==10){document.getElementById('time-end').play();};
+		if(time==0){clearInterval(run_time);
+					document.getElementById('ost').pause();
+					document.getElementById('lose-music').play();
+					$('.lose_page').css('display','block');
+				};
+		},1000);
+});
+/***********  Logic trò chơi   ************/
 var prev = null;
 var score = 0;
 function flip(a){
@@ -47,21 +74,24 @@ if(!prev){
 				function(){
 					document.getElementById('true').play();
 					$(a).css('opacity','0');
-					prev.css('opacity','0');
+					prev.css('opacity','0');					/*********VICOTRY********/
 					$(a).css('pointer-events', 'auto');
 					score++;
-					if(score==9)alert('Victory');
+					if(score==9){
+						clearInterval(run_time);
+						document.getElementById('ost').pause();
+						document.getElementById('victory-music').play();
+						$('.victory_page').css('display','block');
+					};
 					prev = null;
 					},200);
 			}
 	}
 }
 
-$(function(){
-	card = card.concat(card);
-	card = shuffle(card);
-	var display = "";
-	for (var i = 0; i < card.length; i++) {
-		display += '<div class="content">' + '<div class="card" data-name="'+card[i]+'" onclick = "flip(this)">'+'<div class="back-card"><img src="../IMG/'+card[i]+'.jpg"></div>'+'<div class="front-card"><img src="../IMG/card-back2.jpg"></div>'+'</div></div>'};			
-	$("#wrapper").html(display);
-})
+$('.home').click(function(){
+	window.location.href = 'memorycard.html';
+});
+$('.again').click(function(){
+	window.location.href = 'memorycard.html';
+});
