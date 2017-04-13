fabe7d98286bcb1753487753e33315ca0f578e8b
congtrinh0209 <congtrinh0209@gmail.com>
2017-04-14 00:00:30 +0700

    up

--- Du_anTN/JAVASCRIPT/home.js
+++ Du_anTN/JAVASCRIPT/home.js
@@ -4,10 +4,12 @@ $('.item-mobile:nth-child(5)').click(function(){
 	$('.item-mobile:nth-child(6)').css({'opacity':'1','z-index':'3'});
 	$('i.fa.fa-bars').css('display','none');
 	$('.item-dropdown').css('opacity','1')
+	$('.menu-dropdown').css('max-height','360px')
 });
 $('.item-mobile:nth-child(6)').click(function(){
 	$('.icon1').css('transform','rotateZ(0deg)');
 	$('.icon2').css('transform','rotateZ(0deg)');
+	$('.menu-dropdown').css('max-height','0px')
 	$('.item-mobile:nth-child(6)').css({'opacity':'0','z-index':'1'});
 	$('i.fa.fa-bars').css('display','block');
 	$('.item-dropdown').css('opacity','0')
