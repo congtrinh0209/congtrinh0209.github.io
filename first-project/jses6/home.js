2756ba7e43996acedd1b55a57d3e2e2e82f4190c
congtrinh0209 <congtrinh0209@gmail.com>
2017-10-05 21:46:45 +0700

    modify first-project

---
+++ first-project/jses6/home.js
@@ -0,0 +1,242 @@
+/********** JS CHO TRANG MANAGE.HTML - TRANG QUẢN LÝ Load ảnh đại diện  **********/
+	function previewFile() {
+		let preview = document.getElementById('displayImg');
+		let file    = document.querySelector('input[type=file]').files[0];
+		let reader  = new FileReader();
+
+		reader.addEventListener("load", function () {
+			preview.src = reader.result;
+		}, false);
+
+		if (file) {
+			reader.readAsDataURL(file);
+		}
+	}
+$(document).ready(function() {
+    //*************Check login//demo 2 user_data
+        //****Lấy dữ liệu từ JSON - Gán cho biến
+    let data_json;
+    $.getJSON("./data.json", (data)=>{
+        data_json = data;
+        return data_json;
+    });
+        //***Check login
+    $(".login").on("click", ()=>{
+        $.each(data_json.users,(key, value)=>{
+            if($("#uname").val() == value.mobile && $("#psw").val() == value.pass || $("#uname").val() == value.email && $("#psw").val() == value.pass){
+                $("#uname").next("span").text("");
+                $("#psw").next("span").text("");
+                $(".error3").css("padding", "0px");
+                $(".error1").css("padding", "0px");
+                $("#sign-in").submit();
+                return true;
+            }
+            else {
+                $("#psw").next("span").html(`<i class="fa fa-exclamation-circle"></i>Tên đăng nhập hoặc mật khẩu không chính xác</br>`);
+            }
+        });
+        return false;
+    });
+		/***Gán dữ liệu vào menu search khi chọn loại hình tim kiem******/
+		$(".room").on('click',function(){
+			$("#arrow1").css("display", "inline-block");
+			$(".loaihinh").val($(".loaihinh>option:nth-child(2)").val());
+			$("#search-target").text("Phòng cho thuê");
+		});
+		$(".house").on('click',function(){
+			$("#arrow1").css("display", "inline-block");
+			$(".loaihinh").val($(".loaihinh>option:nth-child(3)").val());
+			$("#search-target").text("Nhà cho thuê");
+		});
+		$(".flat").on('click',function(){
+			$("#arrow1").css("display", "inline-block");
+			$(".loaihinh").val($(".loaihinh>option:nth-child(4)").val());
+			$("#search-target").text("Căn hộ cho thuê");
+		});
+		$(".store").on('click',function(){
+			$("#arrow1").css("display", "inline-block");
+			$(".loaihinh").val($(".loaihinh>option:nth-child(5)").val());
+			$("#search-target").text("Mặt bằng cho thuê");
+		});
+		let window_width = innerWidth;
+		if (window_width <= 768) {
+			$(".header").attr("data-offset-top", "89");
+		}
+		//*****Hiển thị sign-in, sign-up
+		if ($(location).attr("href").search("psw")>0){
+			$(".navbar-right").css("display", "none");
+			$(".info-acc").css("display", "block");
+		}
+		$(".sign-out").on("click",()=>{
+			window.location.href = "home.html";
+		});
+        //********Request send password
+    $("#send").on("click",()=> {
+        $.each(data_json.users,(key, value)=>{
+            if ($("#send-mobile").val() == value.mobile) {
+                $("#send-mobile").next("span").text("");
+                $(".error3").css("padding", "0px");
+                alert("Vui lòng kiểm tra tin nhắn để nhận lại mật khẩu!");
+                $("#request-pass").submit();
+                return true;
+            }
+            else {
+                $("#send-mobile").next("span").html(`<i class="fa fa-exclamation-circle"></i> Số điện thoại không tồn tại trên hệ thống!</br>`);
+            }
+        });
+        return false;
+    });
+    //*************Load các quận huyện theo địa danh - menu search
+    function data_locale(data){
+        $.each(data, (key, value)=>{
+            display_locale += `<option>${value}</option>`;
+        });
+    }
+    function load_locale(){
+        let display_locale = "<option>Quận, Huyện</option>";
+        if ($(".locale-item").val() == "Hà Nội"){
+            $.each(data_json.district_locale.district_hn,(key,value)=> {
+                display_locale += `<option>${value}</option>`;
+            });
+        }
+        else if ($(".locale-item").val() == "Đà Nẵng"){
+            $.each(data_json.district_locale.district_dn,(key,value)=> {
+                display_locale += `<option>${value}</option>`;
+            });
+        }
+        else if ($(".locale-item").val() == "TP.Hồ Chí Minh"){
+            $.each(data_json.district_locale.district_hcm,(key,value)=> {
+                display_locale += `<option>${value}</option>`;
+            });
+        }
+        else {
+            display_locale += "<option>Quận, Huyện</option>";
+        }
+        $(".locale_district").html(display_locale);
+    }
+    load_locale();
+    $(".locale-item").click(function() {
+        load_locale();
+    });
+    // JS load tin cho trang tin tức
+    for (let i = 1; i <= 5; i++){
+        $(`#news${i}`).click(() => {
+            $.get("news_detail.html", function (data) {
+                $("#data-new").html(data);
+                $("#data-new").html($(`#new${i}`).html());
+            });
+        });
+    }
+	/********** Trạng thái bài đăng DEMO CHO 2 BÀI ĐĂNG**********/
+	let date_current = new Date();
+	let msec_date_current = Date.parse(date_current);
+  //demo date 1
+	let date_st1 = new Date("2017-05-19");
+	function date_status(date_st1){
+		let msec_dateup1 = Date.parse(date_st1);
+		let msec_datedead1 = msec_dateup1 + (5*86400000);//Date_dead = date_up + 5days
+		let date_dead1 = new Date(msec_datedead1);
+		$(".date-up.item1").html(date_st1.getDate()+"-"+(date_st1.getMonth()+1)+"-"+date_st1.getFullYear());
+		$(".date-dead.item1").html(date_dead1.getDate()+"-"+(date_dead1.getMonth()+1)+"-"+date_dead1.getFullYear());
+		//So sánh ngày hết hạn và thời gian hiện tại
+		if (msec_datedead1 < msec_date_current) {
+			$('#status').html("Tin đã hết hạn");
+			$('#status').css("color","red");
+		}
+	}
+  //demo date 2
+	let date_st2 = new Date("2017-04-28");
+	function date_status2(date_st2){
+		let msec_dateup2 = Date.parse(date_st2);
+		let msec_datedead2 = msec_dateup2 + (5*86400000);//Date_dead = date_up + 5days
+		let date_dead2 = new Date(msec_datedead2);
+		$(".date-up.item2").html(date_st2.getDate()+"-"+(date_st2.getMonth()+1)+"-"+date_st2.getFullYear());
+		$(".date-dead.item2").html(date_dead2.getDate()+"-"+(date_dead2.getMonth()+1)+"-"+date_dead2.getFullYear())
+		//So sánh ngày hết hạn và thời gian hiện tại
+		if (msec_datedead2 < msec_date_current) {
+			$('#status2').html("Tin đã hết hạn")
+		}
+	}
+	window.onload = date_status2(date_st2);
+	window.onload = date_status(date_st1)
+//Thao tác dừng đăng tin và đăng lại tin
+	$("#st1").click(function(){
+		$('#status').html("Tin đã ngừng đăng");
+		$('#status').css("color","#ED2327");
+		$(".date-up.item1").html("");
+		$(".date-dead.item1").html("")
+	});
+
+	$("#st2").click(function(){
+		date_st1 = date_current;//cập nhật ngày đăng theo ngày gia hạn
+		date_status(date_st1);
+		$('#status').html("Tin đang được đăng");
+		$('#status').css("color","#4CAF50");
+	});
+
+	$("#st1-2").click(function(){
+		$('#status2').html("Tin đã ngừng đăng");
+		$('#status2').css("color","#ED2327");
+		$(".date-up.item2").html("");
+		$(".date-dead.item2").html("")
+	});
+	$("#st2-2").click(function(){
+		date_st2 = date_current;//cập nhật ngày đăng theo ngày gia hạn
+		date_status2(date_st2);
+		$('#status2').html("Tin đang được đăng");
+		$('#status2').css("color","#4CAF50");
+	})
+	/// Demo tính năng xem ảnh dạng 360 tour sử dụng Pannellum API
+	pannellum.viewer("panorama1", {
+		"default": {
+			"firstScene": "circle",
+			"author": "CongTrinh0209"
+		},
+		"scenes": {
+			"circle": {
+				"title": "Hình ảnh mang tính minh họa",
+				"hfov": 110,
+				"pitch": -3,
+				"yaw": -50,
+				"type": "equirectangular",
+				"panorama": "https://raw.githubusercontent.com/congtrinh0209/first_project/master/app/images/img-360/PANO_20170601_155002_1.jpg",
+				"autoRotate": -3,
+				"hotSpots": [
+					{
+						"pitch": -3,
+						"yaw": -85,
+						"type": "scene",
+						"text": "Phòng học",
+						"sceneId": "phonghoc",
+						"targetYaw": -50,
+						"targetPitch": 0
+					}
+				]
+			},
+
+			"phonghoc": {
+				"title": "Phòng học",
+				"hfov": 100,
+				"pitch": -50,
+				"yaw": 80,
+				"type": "equirectangular",
+				"panorama": "https://raw.githubusercontent.com/congtrinh0209/first_project/master/app/images/img-360/PANO_20170601_162248_3.jpg",
+				"autoLoad": true,
+				"autoRotate": -3,
+				"hotSpots": [
+					{
+						"pitch": 0,
+						"yaw": -60,
+						"type": "scene",
+						"text": "Phòng chính",
+						"sceneId": "circle",
+						"targetYaw": -48,
+						"targetPitch": 0
+					}
+				]
+			}
+		}
+	});
+	$(".panorama>.pnlm-load-button").html("<p>Click để<br>xem<br>ảnh 360</p><p></p>");
+});
+
---
+++ first-project/jses6/pagination.js
@@ -0,0 +1,259 @@
+/******** PAGINATION ************/
+$(document).ready(function() {
+    $.getJSON("./data.json", (data) => {
+        const length_arr = data.news.length;  // Kích thước mảng dữ liệu tin tức
+        const nper_page = 2;                  // Số tin trên 1 trang
+        var stt_b;                            // Số thứ tự box giữa pagination
+        var number_page = Math.ceil(length_arr / nper_page); // Tính số trang
+        function pagination(a, b) {            // Hàm render html cho mỗi trang
+            let content_new = "";
+            $.each(data.news, (key, value) => {
+                if (a <= key && key < b) {
+                    // HTML template phần nội dung tin đăng
+                    content_new += `<div class="content" id="content${key + 1}">
+                <div class="content-left col-xs-12 col-sm-5 col-md-4">
+                    <div id="myCarousel${key + 1}" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel${key + 1}" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel${key + 1}" data-slide-to="1"></li>
+                            <li data-target="#myCarousel${key + 1}" data-slide-to="2"></li>
+                            <li data-target="#myCarousel${key + 1}" data-slide-to="3"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="./images/img-p${key + 1}/1.jpg" alt="anh1" style="width:100%;">
+                            </div>
+                            <div class="item">
+                                <img src="./images/img-p${key + 1}/2.jpg" alt="anh2" style="width:100%;">
+                            </div>
+                            <div class="item">
+                                <img src="./images/img-p${key + 1}/3.jpg" alt="anh3" style="width:100%;">
+                            </div>
+                            <div class="item">
+                                <img src="./images/img-p${key + 1}/4.jpg" alt="anh3" style="width:100%;">
+                            </div>
+                        </div>
+                        <a class="left carousel-control" href="#myCarousel${key + 1}" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel${key + 1}" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="content-right col-xs-12 col-sm-7 col-md-8">
+                    <div class="content-right1 col-xs-12 col-md-8">
+                    <div class="item-cr1 title">
+                      <a data-toggle="collapse" data-target="#content-detail${key + 1}">${value.title}</a>
+                    </div>
+                    <div class="item-cr1 summarize-desc">
+                      <p>${value.summary}</p>
+                    </div>
+                    <div class="item-cr1 local">Khu vực: <span>${value.locale}</span></div>
+                    <div class="item-cr1 area">Diện tích: <span>${value.area}</span></div>
+                    <div class="item-cr1 date">Ngày đăng: <span>${value.post_date}</span></div>
+                    </div>
+                    <div class="content-right2 col-xs-12 col-md-4">
+                    <p class="hide_op">GIÁ DỊCH VỤ:</p>
+                    <ul>
+                        <li class="item-cr1 water-price hide_op"><i class="fa fa-shower"></i> Nước: <span>${value.price_w}</span></li>
+                        <li class="item-cr1 electric-price hide_op"><i class="fa fa-bolt"></i> Điện: <span>${value.price_e}</span></li>
+                        <li class="item-cr1 internet-price hide_op"><i class="fa fa-wifi"></i> Mạng: <span>${value.price_i}</span></li>
+                        <li class="item-cr1 park-price hide_op"><i class="fa fa-motorcycle"></i> Gửi xe: <span>${value.price_p}</span></li>
+                        <li class="item-cr1 price"><i class="fa fa-money"></i> Phòng: <span>${value.price}</span></li>
+                        <li class="item-cr1 phone"><i class="fa fa-phone"></i> LH: <span>${value.phone}</span></li>
+                    </ul>
+                    <button class="button-detail" data-toggle="collapse" data-target="#content-detail${key + 1}"><i class="fa fa-arrow-right"></i> Xem chi tiết</button>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="clear"></div>
+                <!--Thông tin xem chi tiết-->
+                <article id="content-detail${key + 1}" class="content-detail collapse">
+                    <div class="menu-detail"><!-- Navs Tabs-->
+                        <button class="btn-close button-detail" data-toggle="collapse" data-target="#content-detail${key + 1}">
+                            <i class="fa fa-times"></i> Đóng
+                        </button>
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle="tab" href="#menu1_${key + 1}">THÔNG TIN CHUNG</a></li>
+                            <li><a data-toggle="tab" href="#menu2_${key + 1}">MÔ TẢ CHI TIẾT</a></li>
+                            <li><a data-toggle="tab" href="#menu3_${key + 1}">HÌNH ẢNH</a></li>
+                            <li class = "img-360"><a data-toggle="tab" href="#menu5_${key + 1}">ẢNH 360<sup>o</sup></a></li>
+                            <li class="position item${key + 1}"><a data-toggle="tab" href="#menu4_${key + 1}">VỊ TRÍ</a></li>
+                        </ul>
+                        <!-- Content navs tabs-->
+                        <div class="tab-content">
+                            <div id="menu1_${key + 1}" class="tab-pane fade in active" style="overflow-x:auto;">
+                                <table class="table table-bordered">
+                                    <tbody>
+                                        <tr>
+                                            <td style="font-weight: bold">Địa chỉ</td>
+                                            <td colspan="3" >${value.add}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Loại hình cho thuê</th>
+                                            <td>Chung cư mini</td>
+                                            <th>Ngày cập nhật</th>
+                                            <td>${value.post_date}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Người đăng</th>
+                                            <td>Trịnh Công Trình</td>
+                                            <th>Điện thoại</th>
+                                            <td>${value.phone}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Email</th>
+                                            <td>congtrinh0209@gmail.com</td>
+                                            <th>ADSDSDAS</th>
+                                            <td>ddddddđ</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Diện tích</th>
+                                            <td>${value.area}</td>
+                                            <th>Giá cho thuê</th>
+                                            <td>${value.price}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div id="menu2_${key + 1}" class="tab-pane fade">
+                                <div class="des-detail-content">
+                                  <p>${value.content}</p>
+                                </div>
+                            </div>
+                            <div id="menu3_${key + 1}" class="tab-pane fade">
+                                <div class="image-item">
+                                    <div id="myCarousel2_${key + 1}" class="carousel slide slide2" data-ride="carousel">
+                                        <!-- Indicators -->
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#myCarousel2_${key + 1}" data-slide-to="0" class="active"></li>
+                                            <li data-target="#myCarousel2_${key + 1}" data-slide-to="1"></li>
+                                            <li data-target="#myCarousel2_${key + 1}" data-slide-to="2"></li>
+                                            <li data-target="#myCarousel2_${key + 1}" data-slide-to="3"></li>
+                                        </ol>
+                                        <!-- Wrapper for slides -->
+                                        <div class="carousel-inner" role="listbox">
+                                            <div class="item active">
+                                                <img src="./images/img-p${key + 1}/1.jpg" alt="anh1" style="height: 400px; width: 100%">
+                                            </div>
+                                            <div class="item">
+                                                <img src="./images/img-p${key + 1}/2.jpg" alt="anh2" style="height: 400px; width: 100%">
+                                            </div>
+                                            <div class="item">
+                                                <img src="./images/img-p${key + 1}/3.jpg" alt="anh3" style="height:400px;width: 100%">
+                                            </div>
+                                            <div class="item">
+                                                <img src="./images/img-p${key + 1}/4.jpg" alt="anh4" style="height: 400px; width: 100%">
+                                            </div>
+                                        </div>
+                                        <!-- Left and right controls -->
+                                        <a class="left carousel-control" href="#myCarousel2_${key + 1}" role="button" data-slide="prev">
+                                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="right carousel-control" href="#myCarousel2_${key + 1}" role="button" data-slide="next">
+                                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="menu4_${key + 1}" class="tab-pane fade" style="position: relative;">
+                                <div class="des-detail-content">
+                                    <p style="color: green">Click vào địa điểm để nhận chỉ đường!</p>
+                                    <div id="googleMap${key + 1}" style="width:100%;height:400px;"></div>
+                                    <div class="right-panel" id="right-panel${key + 1}">
+                                        <select class="near_by_item" id="select_item${key + 1}">
+                                            <option>Địa điểm lân cận</option>
+                                            <option>Trường CĐ, ĐH</option>
+                                            <option>Điểm buýt</option>
+                                            <option>Bệnh viện</option>
+                                            <option>Bãi gửi xe</option>
+                                        </select>
+                                        <ul class="places" id="place${key + 1}"></ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="menu5_${key + 1}" class="tab-pane fade">
+                                <p style="color: green">Tính năng này hiện chỉ hỗ trợ trên Chrome Browser</p>
+                                <div id="panorama${key + 1}" class="panorama"></div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>`;
+                    $(".wrap-content").html(content_new);
+                }
+            });
+        }
+        pagination(0, nper_page);
+        if (number_page <= 5){
+            $(".page").each((key, value) => {
+                if (key > number_page - 1) {
+                    $(value).css("display", "none");
+                }
+            });
+            $(".first").addClass("disabled");
+            $(".last").addClass("disabled");
+        }
+        //Hàm phân số thứ tự trang
+        function stt_page() {
+            $(".page0").children().html(stt_b);
+            $(".page_2").children().html(stt_b - 2);
+            $(".page_1").children().html(stt_b - 1);
+            $(".page1").children().html(stt_b + 1);
+            $(".page2").children().html(stt_b + 2);
+        }
+        $(".page>a").click(function() {
+            let page_first = Number($(".page_2>a").html());
+            let page_last = Number($(".page2>a").html());
+            let page = Number(this.innerText);
+            let a = (page - 1) * nper_page;
+            let b = page * nper_page;
+            $(".page").removeClass("active");
+            if ((page > 2) && (page < number_page - 1)) {
+                stt_b = page;
+                stt_page();
+                $(".page0").addClass("active");
+                pagination(a, b);
+            }
+            // Trường hợp trang thứ 2 và trang gần cuối
+            else if (page == 2 && page_first == page ){
+                $(".page>a").each((index, value) => {
+                    $(value).html((Number($(value).html()) - 1));
+                });
+                $(".page_1").addClass("active");
+                pagination(a, b);
+            }
+            else if (page == (number_page - 1) && page == page_last){
+                $(".page>a").each((index, value) => {
+                    $(value).html((Number($(value).html()) + 1));
+                });
+                $(".page1").addClass("active");
+                pagination(a, b);
+            }
+            else {
+                $(this).parent().addClass("active");
+                pagination(a, b);
+            }
+        });
+        // Hàm load cho First-box và Last-box
+        $(".first>a").click(function () {
+            stt_b = 3;
+            stt_page();
+            pagination(0, nper_page);
+            $(".page").removeClass("active");
+            $(".page_2").addClass("active");
+        });
+        $(".last>a").click(function () {
+            stt_b = number_page -2;
+            stt_page();
+            pagination((number_page-1)*nper_page, length_arr);
+            $(".page").removeClass("active");
+            $(".page2").addClass("active");
+        });
+    });
+});
