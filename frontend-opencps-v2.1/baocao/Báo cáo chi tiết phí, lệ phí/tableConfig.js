7b050c34ec2249f6c363346a516cdb7e2cb143ce
congtrinh0209 <congtrinh0209@gmail.com>
2020-06-12 12:19:12 +0700

    update

--- frontend-opencps-v2.1/baocao/Báo cáo chi tiết phí, lệ phí/tableConfig.js
+++ frontend-opencps-v2.1/baocao/Báo cáo chi tiết phí, lệ phí/tableConfig.js
@@ -1,115 +1,114 @@
 
 {
-    docDefinition: {
-       pageOrientation: 'landscape',
-       content: [
-         {
-               columns: [
-                   {
-                       width: 300,
-                       style: "title",
-                       text: [
-                          {text: "[$groupIds$]\n"},
-                         {text: "--------------------------------\n"}
-                       ]
+  docDefinition: {
+     pageOrientation: 'landscape',
+     content: [
+       {
+             columns: [
+                 {
+                     width: 300,
+                     style: "title",
+                     text: [
+                        {text: "[$groupIdsHead$]\n"},
+                       {text: "--------------------------------\n"}
+                     ]
+                 },
+                 {
+                     width: 100,
+                      style: "title",
+                     text: ''
                    },
-                   {
-                       width: 100,
-                            style: "title",
-                       text: ''
-                     },
-                   {
-                       width: 300,
-                       style: "title",
-                       text: [
-                           {text: "CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM\n"},
-                           {text: "Độc lập - Tự do - Hạnh phúc\n"},
-                           {text: "----------------------------\n"},
-                           {text: "Hậu Giang, ngày " + new Date().getDate() + " tháng " + (new Date().getMonth() + 1) +" năm " + new Date().getFullYear(), style: "ngayThangNam"}
-                       ]
-                   }
-               ]
+                 {
+                     width: 300,
+                     style: "title",
+                     text: [
+                         {text: "CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM\n"},
+                         {text: "Độc lập - Tự do - Hạnh phúc\n"},
+                         {text: "----------------------------\n"},
+                         {text: "Hậu Giang, ngày " + new Date().getDate() + " tháng " + (new Date().getMonth() + 1) +" năm " + new Date().getFullYear(), style: "ngayThangNam"}
+                     ]
+                 }
+             ]
+       },
+       {
+         text: [
+           {text: 'BÁO CÁO CHI TIẾT PHÍ, LỆ PHÍ\n'},
+           {text: "Đơn vị: [$groupIds$]\n", fontSize: 11},
+           {text: "Từ ngày [$fromReceiveDate$] đến ngày [$toReceiveDate$]\n", fontSize: 11}
+         ], 
+         margin: [0, 20],
+         style: 'headerTitle'
+       },
+       {
+         style: 'tableExample',
+         table: {
+           widths: "[$tableWidth$]",
+           body: ["[$report$]"]
          },
-         {
-           text: [
-             {text: 'BÁO CÁO CHI TIẾT PHÍ, LỆ PHÍ\n'},
-             {text: "Đơn vị: [$groupIds$]\n", fontSize: 11},
-             {text: "Từ ngày [$fromReceiveDate$] đến ngày [$toReceiveDate$]\n", fontSize: 11}
-           ], 
-           margin: [0, 20],
-           style: 'headerTitle'
-         },
-         {
-           style: 'tableExample',
-           table: {
-             widths: "[$tableWidth$]",
-             body: ["[$report$]"]
+         margin: [0, 0, 0, 20]
+       },
+       {
+         columns: [
+           {
+             width: '*',
+             text: ''
            },
-           margin: [0, 0, 0, 20]
-         },
-         {
-           columns: [
-             {
-               width: '*',
-               text: ''
-             },
-             {
-               width: 150,
-               style: 'title',
-               text: 'NGƯỜI TIẾP NHẬN HỒ SƠ'
-             }
-           ]
-         },
-         {
-           columns: [
-             {
-               width: '*',
-               text: ''
-             },
-             {
-               width: 150,
-               style: 'titleSub',
-               text: '(Ký và ghi rõ họ tên)'
-             }
-           ]
-         }
-       ],
-       styles: {
-         ngayThangNam: {
+           {
+             width: 150,
+             style: 'title',
+             text: 'NGƯỜI TIẾP NHẬN HỒ SƠ'
+           }
+         ]
+       },
+       {
+         columns: [
+           {
+             width: '*',
+             text: ''
+           },
+           {
+             width: 150,
+             style: 'titleSub',
+             text: '(Ký và ghi rõ họ tên)'
+           }
+         ]
+       }
+     ],
+     styles: {
+       ngayThangNam: {
+         fontSize: 10,
+         italics: true,
+         bold: false,
+         alignment: 'center'
+       },
+       headerTitle: {
+           alignment: 'center',
+           bold: true,
+           fontSize: 13
+       },
+       borderHeader: {
+           alignment: 'left'
+       },
+       title: {
+         bold: true,
+         alignment: 'center',
+         textTransform: 'uppercase'
+       },
+       titleSub: {
            fontSize: 10,
            italics: true,
-           bold: false,
            alignment: 'center'
-         },
-         headerTitle: {
-             alignment: 'center',
-             bold: true,
-             fontSize: 13
-         },
-         borderHeader: {
-             alignment: 'left'
-         },
-         title: {
-           bold: true,
-           alignment: 'center',
-           textTransform: 'uppercase'
-         },
-         titleSub: {
-             fontSize: 10,
-             italics: true,
-             alignment: 'center'
-         },
-         tdStyle: {
-           fontSize: 10
-         },
-         tableExample: {
-            width: "100%" 
-         }
        },
-       defaultStyle: {
-         columnGap: 20,
-         fontSize: 11
+       tdStyle: {
+         fontSize: 10
+       },
+       tableExample: {
+          width: "100%" 
        }
-     } 
- }
- 
+     },
+     defaultStyle: {
+       columnGap: 20,
+       fontSize: 11
+     }
+   } 
+}
